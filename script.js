80159a41ae119b31ab14f669f56d72cf26c18024
James Keech <jameskeech@Jamess-MacBook-Pro.local>
2021-10-22 15:31:00 +1100

    Created addMove function to allow users to select a move.

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+
+
+const gameSquare = document.querySelectorAll('.gameBoardSquare');
+
+
+
+// console.log(gameSquare);
+
 const GameBoard = (() => {
-    var board = ['x', 'o', 'x', 'x', 'o', 'x', 'x', 'o', 'x'];
+    var board = [];
+    // const addMove = (square, player) => {
+    //     if (player === 'player 1') {
+    //         board[square] = 'X'
+    //     }
+    //     else {
+    //         board[square] = 'O'
+    //     }
+    // }
     return {
         board
     }
@@ -9,19 +25,47 @@ const GameBoard = (() => {
 const Player = (name) => {
     const getName = () => name;
 
+    const move = (getName) => {
+
+
+    }
+    
     return {getName}
 };
 
-function displayController() {
+function gameController() {
     var current = GameBoard.board;
     
     for (let i=0; i < current.length; i++) {
         var thisMove = document.getElementById(i);
         //thisMove.innerHTML(current[i]);
-        console.log(thisMove);
+        if (typeof current[i] !== 'undefined')
         thisMove.innerHTML = current[i];
     }
    
 }
 
-displayController();
+gameController();
+
+const player1 = Player('Player 1');
+const player2 = Player('Player 2');
+var currentPlayer = player1;
+
+function addMove (player, square) {
+
+    if (player === 'Player 1') {
+        GameBoard.board[square] = 'X';
+        currentPlayer = player2;
+        console.log(currentPlayer.getName());
+    }
+    else if (player === 'Player 2') {
+        GameBoard.board[square] = 'O'
+        currentPlayer = player1;
+    }
+
+    
+    gameController();
+}
+
+gameSquare.forEach(square => square.addEventListener('click', () => addMove(currentPlayer.getName(), square.id)));
+
